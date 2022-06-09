5356e71a8bd260f87d77326a95a37967215d21d8
Adam <adam.weissert@sru.edu>
2022-06-08 21:40:43 -0400

    initial commit

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let todos = [{id: 1, name: 'Cohen Interview Assignment'}];
+let todos = [{ id: 1, name: 'Cohen Interview Assignment' }];
 
 const PORT = process.env.PORT || 3000
 
 app.get('/todos', (req, res) => {
-  return res.status(200).send(todos);
+    return res.status(200).send(todos);
+});
+
+app.get('/tasks', (req, res) => {
+    return res.status(200).send(todos);
+});
+
+app.post('/todos', (req, res) => {
+    return res.status(200).send(todos);
+});
+
+app.post('/tasks', (req, res) => {
+    return res.status(200).send(todos);
 });
 
 app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
+    console.log(`Example app listening on port ${PORT}!`),
 );
